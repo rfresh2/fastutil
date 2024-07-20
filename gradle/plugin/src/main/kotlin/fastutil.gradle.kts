0d8793c4be2c8e97be5810168dad358305c6f0b6
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-07-20 14:34:28 -0700

    setup maven publishing

--- gradle/plugin/src/main/kotlin/fastutil.gradle.kts
+++ gradle/plugin/src/main/kotlin/fastutil.gradle.kts
@@ -14,9 +14,26 @@ java {
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "vc"
+            url = uri("https://maven.2b2t.vc/releases")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+            version = project.version.toString()
+            System.getenv("PUBLISH_VERSION")?.let {
+                version = it
+            }
             pom {
                 packaging = "jar"
                 url.set("https://github.com/rfresh2/fastutil")
